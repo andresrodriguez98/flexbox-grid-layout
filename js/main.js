c80a3495fe5f64dad087acfa5de766ee17bb5932
andresrodriguez98 <dev.andresrodriguez@gmail.com>
2024-01-22 17:23:31 -0500

    se maquetea y se estiliza el contenido de la pagina flexbox-grid-layout

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const photoContainer = document.getElementById("photo-container");
+
+let content = "<div class='column'>";
+
+for(let i=0; i<15; i++){
+
+    if(i!=0 && (i % 5 == 0)){
+        content += `</div><div class="column">
+        ${getphotoElement(i+1)}`
+
+    }else{
+        content += getphotoElement(i+1)
+    }
+}
+
+content += "</div";
+
+photoContainer.innerHTML = content;
+
+function getphotoElement(photo_id){
+    return `<a href="#" class"photo">
+    <img alt="photo ${photo_id}" src="img/photo_${photo_id}.jpg"/>
+    </a>`;
+}

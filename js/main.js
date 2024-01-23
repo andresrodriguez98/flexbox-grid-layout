8306f860f8c24f3f523642b4dcbf4a1971d7db78
andresrodriguez98 <dev.andresrodriguez@gmail.com>
2024-01-23 14:40:28 -0500

    se termina proyecto usando flexbox-grid-layout

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const photoContainer = document.getElementById("photo-container");
 
 let content = "<div class='column'>";
 
-for(let i=0; i<15; i++){
+for(let i=0; i<20; i++){
 
     if(i!=0 && (i % 5 == 0)){
         content += `</div><div class="column">
@@ -13,12 +13,12 @@ for(let i=0; i<15; i++){
     }
 }
 
-content += "</div";
+content += "</div>";
 
 photoContainer.innerHTML = content;
 
 function getphotoElement(photo_id){
-    return `<a href="#" class"photo">
+    return `<a href="#" class="photo">
     <img alt="photo ${photo_id}" src="img/photo_${photo_id}.jpg"/>
     </a>`;
 }
